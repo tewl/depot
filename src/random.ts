28dd0090e2f5c9b36c485b5cdd769049c0fc23b6
Kevin Peters <kwpeters@gmail.com>
2018-09-30 02:22:07 -0400

    Added random.ts and unit tests.

---
+++ src/random.ts
@@ -0,0 +1,57 @@
+//
+// Note
+// ----
+// The following functions were taken from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// These functions do not produce cryptographically string random values.
+//
+// To get cryptographically string random numbers, use window.crypto.getRandomValues().
+//
+
+
+/**
+ * Returns a random (floating point) number between 0 (included) and 1 (excluded)
+ * @returns {number} The generated random floating point number
+ */
+export function getRandom(): number {
+    "use strict";
+    return Math.random();
+}
+
+
+/**
+ * Returns a random (floating point) number.
+ * @param minIncluded - The minimum possible value
+ * @param maxExcluded - The maximum value
+ * @returns {number} The generated random floating point number
+ */
+export function getRandomFloat(minIncluded: number, maxExcluded: number): number {
+    "use strict";
+    return Math.random() * (maxExcluded - minIncluded) + minIncluded;
+}
+
+
+/**
+ * Returns a random integer between minIncluded and maxExcluded
+ * @param minIncluded - The minimum possible value
+ * @param maxExcluded - The maximum possible value
+ * @returns {number} The generated random integer
+ */
+export function getRandomInt(minIncluded: number, maxExcluded: number): number {
+    "use strict";
+    // Using Math.round() will give you a non-uniform distribution!
+    return Math.floor(Math.random() * (maxExcluded - minIncluded)) + minIncluded;
+}
+
+
+/**
+ * Returns a random integer between minIncluded and maxIncluded
+ * @param minIncluded - The minimum possible value
+ * @param maxIncluded - The maximum possible value
+ * @returns {number} The generated random integer
+ */
+export function getRandomIntInclusive(minIncluded: number, maxIncluded: number): number {
+    "use strict";
+    // Using Math.round() will give you a non-uniform distribution!
+    return Math.floor(Math.random() * (maxIncluded - minIncluded + 1)) + minIncluded;
+}

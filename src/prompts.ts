713ae2971dbfa152aed5e0b9707a15fb46388ba7
Kevin Peters <kwpeters@gmail.com>
2018-11-02 14:54:29 -0400

    Added ability to set default answer in promptToContinue().

--- src/prompts.ts
+++ src/prompts.ts
@@ -4,16 +4,23 @@ import * as inquirer from "inquirer";
 /**
  * Prompts the user to confirm whether they want to continue.
  * @param message - The message to display
+ * @param defaultToConfirm - true to make confirmation the default response.
+ * false to make canceling the default.
  * @param resolveValue - If the user chooses to continue, the returned promise
  * will be resolved with this value.  This makes promise chains easier to
  * create.
  * @return A promise that is resolved with resolveValue when the user chooses to
  * continue and rejects when they decline
  */
-export function promptToContinue<T>(message: string, resolveValue: T): Promise<T> {
+export function promptToContinue<T>(
+    message: string,
+    defaultToConfirm: boolean,
+    resolveValue: T
+): Promise<T> {
     const questionConfirmation = {
         type: "confirm",
         name: "confirm",
+        default: defaultToConfirm,
         message: message || "Continue?"
     };
 

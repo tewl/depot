2bec28c8dbe50704424b25d63911783e4f89a998
Kevin Peters <kwpeters@gmail.com>
2019-01-25 20:23:08 -0500

    Improved unit test readability.

--- src/networkHelpers.spec.ts
+++ src/networkHelpers.spec.ts
@@ -6,9 +6,8 @@ describe("getExternalIpv4Addresses()", () => {
 
     it("will return an object with at least one string key", () => {
         const networkAddresses = getExternalIpv4Addresses();
-        const firstKey = Object.keys(networkAddresses)[0];
-        expect(_.isString(networkAddresses[firstKey])).toEqual(true);
-        console.log("networkAddresses =", JSON.stringify(networkAddresses, undefined, 4));
+        const firstIpAddr = _.values(networkAddresses)[0];
+        expect(_.isString(firstIpAddr)).toEqual(true);
     });
 
 

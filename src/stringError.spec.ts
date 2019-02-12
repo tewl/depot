9454fa056aeedbcba228e09920eef94976720c36
Kevin Peters <kwpeters@gmail.com>
2019-02-12 16:57:11 -0500

    Added StringError.

---
+++ src/stringError.spec.ts
@@ -0,0 +1,113 @@
+import * as BBPromise from "bluebird";
+import {isStringError, StringError} from "./stringError";
+import {assertNever} from "./never";
+
+
+// A union of string literals that represent all possible errors.
+type BuildCastleError = "BURNED_DOWN" | "FELL_OVER" | "SANK_INTO_SWAMP";
+
+
+function buildCastle(): Promise<void> {
+    return reject("FELL_OVER");
+
+    // A helper function to make sure that we only return errors of the desired
+    // type (since promises always treat errors as `any`).
+    function reject(err: BuildCastleError): Promise<never> {
+        return BBPromise.reject(new StringError<BuildCastleError>(err));
+    }
+}
+
+
+describe("StringError", () => {
+
+
+    it("Will emit compile-time errors as expected", (done) => {
+
+        //
+        // This unit test is here in order to experiment with code and to make
+        // sure the expected compiler errors are observed.  This unit test does
+        // not test any behavior.
+        //
+
+        buildCastle()
+        .catch((err: StringError<BuildCastleError>) => {
+
+            // The following is needed if you don't define the type of err
+            // above.
+            //
+            // if (!isStringError<BuildCastleError>(err)) {
+            //     // Rethrow if we got an unexpected error type.
+            //     throw err;
+            // }
+
+            switch (err.errorCode)
+            {
+                case "BURNED_DOWN":
+                    break;
+
+                case "FELL_OVER":
+                    break;
+
+                // If you comment out the following case...break, there should
+                // be an error on the assertNever() call stating that "Arugment
+                // of type "SANK_INTO_SWAMP" is not assignable to parameter of
+                // type 'never'."
+                case "SANK_INTO_SWAMP":
+                    break;
+
+                default:
+                    assertNever(err.errorCode);
+            }
+
+            done();
+        });
+    });
+
+
+    describe("instances", () => {
+
+        it("are Error objects", () => {
+            const err = new StringError<BuildCastleError>("FELL_OVER");
+            expect(err instanceof Error).toEqual(true);
+        });
+
+
+        it("will have a `errorCode` property set to the specified value", () => {
+            const err = new StringError<BuildCastleError>("FELL_OVER");
+            expect(err.errorCode).toEqual("FELL_OVER");
+        });
+
+
+        it("will have the expected `message` value when one is not specified", () => {
+            const err = new StringError<BuildCastleError>("FELL_OVER");
+            expect(err.message).toEqual("FELL_OVER");
+        });
+
+
+        it("will have the expected `message` value when one is specified", () => {
+            const err = new StringError<BuildCastleError>("FELL_OVER", "foo");
+            expect(err.message).toEqual("foo");
+        });
+
+
+    });
+
+});
+
+
+describe("isStringError()", () => {
+
+
+    it("returns true when given a StringError", () => {
+        const err = new StringError<BuildCastleError>("FELL_OVER");
+        expect(isStringError(err)).toEqual(true);
+    });
+
+
+    it("returns false when given something that is not a StringError", () => {
+        const err = new Error("foo");
+        expect(isStringError(err)).toEqual(false);
+    });
+
+
+});
---
+++ src/stringError.ts
@@ -0,0 +1,48 @@
+// A polyfill for es2015 Object.setPrototypeOf().
+// tslint:disable-next-line:no-require-imports
+import setPrototypeOf = require("setprototypeof");
+
+/**
+ * An error class that wraps a string error code defined in a union type.
+ * A union of string literal types is used because it has the advantage that
+ * TypeScript can check for exhaustiveness.
+ *
+ * For an example of how to best utilize this class, see the associated spec
+ * file.
+ */
+export class StringError<StringLiteralUnionType> extends Error
+{
+    /**
+     * The error string representing the error
+     */
+    public readonly errorCode: StringLiteralUnionType;
+
+
+    /**
+     * Constructs a new StringError instance that wraps the specified string
+     * literal from the templated union type
+     * @param errorString - The error string representing the error
+     * @param message - An optional string further describing the error
+     * conditions.  If not specified, `errorString` will be used.
+     */
+    public constructor(errorString: StringLiteralUnionType, message?: string) {
+        // Chain to the base class.
+        // Unfortunately, there is a weird issue related to extending Error and
+        // targeting ES5.  See: http://bit.ly/2wDu0XP
+        super(message || errorString.toString());
+        setPrototypeOf(this, StringError.prototype);
+
+        this.errorCode = errorString;
+    }
+}
+
+
+/**
+ * User-defined type guard that determines whether `val` is a StringError<T>.
+ * @param val - The value to be tested
+ * @return true if `val` is a StringError<T>. false otherwise.
+ */
+export function isStringError<T>(val: any): val is StringError<T> {
+    return val instanceof Error &&
+           (val as any).errorCode !== undefined;
+}

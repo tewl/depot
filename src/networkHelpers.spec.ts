98785cd7ce73e4409a421f7f3808f5606627bcc1
Kevin Peters <kwpeters@gmail.com>
2019-02-21 10:03:57 -0500

    Added functions that check for available TCP ports.

--- src/networkHelpers.spec.ts
+++ src/networkHelpers.spec.ts
@@ -1,5 +1,8 @@
+import * as net from "net";
 import * as _ from "lodash";
-import {getExternalIpv4Addresses} from "./networkHelpers";
+import * as BBPromise from "bluebird";
+import {getExternalIpv4Addresses, isTcpPortAvailable, getAvailableTcpPort, selectAvailableTcpPort} from "./networkHelpers";
+
 
 describe("getExternalIpv4Addresses()", () => {
 
@@ -8,6 +11,116 @@ describe("getExternalIpv4Addresses()", () => {
         const networkAddresses = getExternalIpv4Addresses();
         const firstIpAddr = _.values(networkAddresses)[0];
         expect(_.isString(firstIpAddr)).toEqual(true);
+    });
+
+
+});
+
+
+describe("isTcpPortAvailable()", () => {
+
+
+    it("resolves to true when the port is available", async () => {
+        expect(await isTcpPortAvailable(33279)).toEqual(true);
+    });
+
+
+    it("resolves to false when the port is not available", (done) => {
+        const port = 33279;
+        const server = net.createServer();
+        server.unref();
+        server.listen({port}, () => {
+            isTcpPortAvailable(port)
+            .then((portIsAvailable) => {
+                expect(portIsAvailable).toEqual(false);
+                server.close(() => {
+                    done();
+                });
+            });
+        });
+    });
+
+
+});
+
+
+describe("getAvailableTcpPort()", () => {
+
+    it("resolves with an available port number", async () => {
+        const port = await getAvailableTcpPort();
+        expect(port).toBeGreaterThan(0);
+        const isAvailable = await isTcpPortAvailable(port);
+        expect(isAvailable).toEqual(true);
+    });
+
+
+});
+
+
+describe("selectAvailableTcpPort()", () => {
+
+    interface IServerInfo {
+        server: net.Server;
+        port: number;
+    }
+
+    // A helper function to start a server.
+    function startServerAtFirstAvailablePort(): Promise<IServerInfo> {
+        return new BBPromise<IServerInfo>((resolve, reject) => {
+            const server = net.createServer();
+            server.unref();
+            server.on("error", reject);
+            server.listen({port: 0}, () => {
+                const {port} = server.address();
+                resolve({server, port});
+            });
+        });
+    }
+
+    // A helper function to shutdown a running server.
+    function shutdownServer(server: net.Server): Promise<void> {
+        return new BBPromise((resolve) => {
+            server.close(() => {
+                resolve();
+            });
+        });
+    }
+
+
+    it("will select a preferred port when it is not in use", async () => {
+        const serverInfo1 = await startServerAtFirstAvailablePort();
+
+        // Start a second server to see what port it gets and then shut it down.
+        // When selecting a port, we will list it as a preferred port and will
+        // expect to get it.
+        const serverInfo2 = await startServerAtFirstAvailablePort();
+        await shutdownServer(serverInfo2.server);
+
+        const selectedPort = await selectAvailableTcpPort(
+            serverInfo1.port,    // should not get this one - still in use
+            serverInfo2.port     // should get this one
+        );
+
+        expect(selectedPort).toBeGreaterThan(0);
+        expect(selectedPort).not.toEqual(serverInfo1.port);
+        expect(selectedPort).toEqual(serverInfo2.port);
+
+        await shutdownServer(serverInfo1.server);
+    });
+
+
+    it("will return the first available port when all preferred ports are in use", async () => {
+        const serverInfo1 = await startServerAtFirstAvailablePort();
+        const serverInfo2 = await startServerAtFirstAvailablePort();
+
+        const selectedPort = await selectAvailableTcpPort(serverInfo1.port, serverInfo2.port);
+
+        expect(selectedPort).toBeGreaterThan(0);
+        expect(selectedPort).not.toEqual(serverInfo1.port);
+        expect(selectedPort).not.toEqual(serverInfo2.port);
+
+        await BBPromise.all([shutdownServer(serverInfo1.server),
+                             shutdownServer(serverInfo2.server)]);
     });
 
 
--- src/networkHelpers.ts
+++ src/networkHelpers.ts
@@ -1,4 +1,7 @@
 import * as os from "os";
+import * as net from "net";
+import * as _ from "lodash";
+import * as BBPromise from "bluebird";
 
 
 /**
@@ -24,4 +27,71 @@ export function getExternalIpv4Addresses(): {[networkInterfaceName: string]: str
     }
 
     return foundInterfaces;
+}
+
+
+/**
+ * Helper function that will determine if the specified port is available.
+ * @param port - The port to test.  Specify 0 if you want to find the first
+ * available port.
+ * @return A promise that resolves with the port when available.  It rejects if
+ * the specified port is not available.
+ */
+function isAvailable(port: number): Promise<number>
+{
+    return new BBPromise<number>((resolve, reject) => {
+        const server = net.createServer();
+        server.unref();
+        server.on("error", reject);
+        server.listen({port}, () => {
+            const {port} = server.address();
+            server.close(() => {
+                resolve(port);
+            });
+        });
+    });
+}
+
+
+/**
+ * Determines whether the specified TCP port is available.
+ * @param port - The port to check
+ * @return A promise that always resolves with a boolean indicating whether the
+ * specified port is available.
+ */
+export function isTcpPortAvailable(port: number): Promise<boolean>
+{
+    return isAvailable(port)
+    .then(() => true)
+    .catch(() => false);
+}
+
+
+/**
+ * Gets an available TCP port number
+ * @return An available TCP port number
+ */
+export function getAvailableTcpPort(): Promise<number>
+{
+    return isAvailable(0);
+}
+
+
+/**
+ * Gets an available TCP port number, choosing from the optional preferred ports
+ * first.  If none of those are available, the first available TCP port is
+ * returned.
+ * @param preferredPorts - Ports that will be checked first to see if they are
+ * available
+ * @return An available TCP port number
+ */
+export function selectAvailableTcpPort(...preferredPorts: Array<number>): Promise<number>
+{
+    return _.reduce<number, Promise<number>>(
+        [...preferredPorts, 0],
+        (acc, curPort) => {
+            return acc.catch(() => isAvailable(curPort));
+        },
+        BBPromise.reject(undefined)
+    );
 }

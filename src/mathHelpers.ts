c8c7c6aa4ae48e70477e8a6abb07bb003dd55020
Kevin Peters <kwpeters@gmail.com>
2019-03-05 10:02:08 -0500

    Added factorial().

---
+++ src/mathHelpers.ts
@@ -0,0 +1,17 @@
+const factorialCache: Array<number> = []
+
+export function factorial(val: number): number
+{
+    if (val === 0 || val === 1) {
+        return 1;
+    }
+    else if (factorialCache[val] !== undefined) {
+        return factorialCache[val];
+    }
+    else {
+        const f = factorial(val - 1) * val;
+        factorialCache[val] = f;
+        return f;
+    }
+
+}

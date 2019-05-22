602d7d4211a0b2bd356465e3f02419511e8a2aca
Kevin Peters <kwpeters@gmail.com>
2019-05-22 09:12:20 -0400

    Added jsdoc

--- src/uniqueProcessStr.ts
+++ src/uniqueProcessStr.ts
@@ -3,6 +3,10 @@ const startTimestamp = Date.now();
 const pid = process.pid;
 
 
+/**
+ * Creates a string that will be unique across this machine.
+ * @return Returns a unique string comprised of a timestamp, pid and counter.
+ */
 export function getUniqueProcessStr(): string
 {
     const uniqueStr = `${startTimestamp}_pid${pid}_${counter}`;

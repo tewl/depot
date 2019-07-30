a852a542d6be452b04420a9798c928f203134c09
Kevin Peters <kwpeters@gmail.com>
2019-07-30 14:26:31 -0400

    Added parameter to toGulpError() for a default message.

--- src/gulpHelpers.ts
+++ src/gulpHelpers.ts
@@ -9,12 +9,17 @@ interface IGulpError extends Error
  * the error's stack trace.
  *
  * @param err - An object describing the error.  If it does not have a `message`
- * property, one will be created with a generic message.
+ *     property, one will be created with a generic message.
+ * @param defaultErrorMsg - The error message to use when `err.message` does not
+ *     exist.
  * @return A Gulp error object that will not display the stack trace
  */
-export function toGulpError(err: any): Error
+export function toGulpError(
+    err: any,
+    defaultErrorMsg: string = "Gulp encountered one or more errors."
+): Error
 {
-    const gulpError: IGulpError = new Error(err.message || "Gulp encountered one or more errors.") as IGulpError;
+    const gulpError: IGulpError = new Error(err.message || defaultErrorMsg) as IGulpError;
     // Don't show the stack trace.
     gulpError.showStack = false;
     return gulpError;

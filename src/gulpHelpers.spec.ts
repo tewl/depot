99dfc958977ffddb405374fcf3f64dc53c76e94d
Kevin Peters <kwpeters@gmail.com>
2019-07-30 14:26:54 -0400

    Added unit tests for toGulpError().

---
+++ src/gulpHelpers.spec.ts
@@ -0,0 +1,25 @@
+import {toGulpError} from "./gulpHelpers";
+
+
+describe("toGulpError", () => {
+
+
+    it("sets showStack to false", () => {
+        const err = new Error("foo");
+        expect(toGulpError(err).showStack).toEqual(false);
+    });
+
+
+    it("uses the message when the error object has one", () => {
+        const err = new Error("foo");
+        expect(toGulpError(err).message).toEqual("foo");
+    });
+
+
+    it("uses the default error message when the error object does not have one", () => {
+        const err = new Error();
+        expect(toGulpError(err).message).toEqual("Gulp encountered one or more errors.");
+    });
+
+
+});

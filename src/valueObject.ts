8a62a624c6be0d6f6f9ab79ead15097f33d71ce1
Kevin Peters <kwpeters@gmail.com>
2020-02-18 10:14:10 -0500

    Added valueObject placeholder for reference.

---
+++ src/valueObject.ts
@@ -0,0 +1,7 @@
+// Adapted from the following article:
+// https://khalilstemmler.com/articles/typescript-value-object/
+//
+// I don't think I need this abstract base class.  I think it's better to just
+// write the individual value object classes.  The implementation of ValueObject
+// provided in the article does not provide much, and its use of any opens up
+// typing issues.

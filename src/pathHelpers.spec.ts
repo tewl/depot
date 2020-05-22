997b497229f277003239e8cdea18c33142315d8d
Kevin Peters <kwpeters@ra.rockwell.com>
2020-05-22 17:51:32 -0400

    Fixed more unit tests on Windows.

--- src/pathHelpers.spec.ts
+++ src/pathHelpers.spec.ts
@@ -1,3 +1,4 @@
+import * as path from "path";
 import * as pathHelpers from "./pathHelpers";
 import {Directory} from "./directory";
 
@@ -7,7 +8,7 @@ describe("reducePathParts()", () => {
 
     it("should join the path parts", () => {
         const resultPath: string = pathHelpers.reducePathParts(["foo", "bar", "baz.txt"]);
-        expect(resultPath).toEqual("foo/bar/baz.txt");
+        expect(resultPath).toEqual(path.join("foo", "bar", "baz.txt"));
     });
 
 
@@ -16,7 +17,7 @@ describe("reducePathParts()", () => {
             "foo",
             new Directory("bar"),
             "baz.txt"]);
-        expect(result).toEqual("bar/baz.txt");
+        expect(result).toEqual(path.join("bar", "baz.txt"));
     });
 
 

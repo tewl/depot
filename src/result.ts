7db1f2047bca2368a70a272060f4ed97cda0f5d8
Kevin Peters <kwpeters@ra.rockwell.com>
2020-08-01 12:21:49 -0400

    Added user-defined type guards for Result objects.

--- src/result.ts
+++ src/result.ts
@@ -11,6 +11,26 @@ export interface ISuccessResult<SuccessType> {
 
 export type Result<SuccessType, ErrorType> = ISuccessResult<SuccessType> | IFailureResult<ErrorType>;
 
+/**
+ * User-defined type guard that determines whether the specified result is a
+ * successful result.
+ * @param result - The result object to inspect.
+ * @return True if `result` is a successful result.
+ */
+export function isSuccess<SuccessType, ErrorType>(result: Result<SuccessType, ErrorType>): result is ISuccessResult<SuccessType> {
+    return result.success;
+}
+
+/**
+ * User-defined type guard that determines whether the specified result is a
+ * failure result.
+ * @param result - The result object to inspect.
+ * @return True if `result` is a failure result.
+ */
+export function isFailure<SuccessType, ErrorType>(result: Result<SuccessType, ErrorType>): result is IFailureResult<ErrorType> {
+    return !result.success;
+}
+
 export function failureResult<ErrorType>(errorId: ErrorType, message?: string): IFailureResult<ErrorType> {
     return {success: false, errorId: errorId, message: message};
 }

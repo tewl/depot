db62910956462ce3e28c9e10a995311e6310e4d4
Kevin Peters <kwpeters@ra.rockwell.com>
2021-05-13 16:18:24 -0400

    Added streamToBuffer().

--- src/streamHelpers.ts
+++ src/streamHelpers.ts
@@ -23,3 +23,22 @@ export function readableStreamToText(readable: Readable): Promise<string> {
 
     });
 }
+
+
+// A helper method used to read a Node.js readable stream into a Buffer
+export function streamToBuffer(readableStream: Readable): Promise<Buffer>
+{
+    return new Promise((resolve, reject) =>
+    {
+        const chunks: Array<Buffer> = [];
+        readableStream.on("data", (data) =>
+        {
+            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        readableStream.on("end", () =>
+        {
+            resolve(Buffer.concat(chunks));
+        });
+        readableStream.on("error", reject);
+    });
+}

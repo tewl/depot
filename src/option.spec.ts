01d84a2c71bfe811f8b707dba830bc2887df9fdc
Kevin Peters <kwpeters@ra.rockwell.com>
2021-07-30 09:47:30 -0400

    Added Option.

---
+++ src/option.spec.ts
@@ -0,0 +1,183 @@
+import { assertNever } from "./never";
+import {bindOption, isNone, isSome, mapSome, noneOption, Option, someOption} from "./option";
+import { pipe } from "./pipe";
+
+////////////////////////////////////////////////////////////////////////////////
+// Test Infrastructure
+
+function someOperation(): Option<number>
+{
+    return someOption(5);
+}
+
+
+function noneOperation(): Option<number>
+{
+    return noneOption();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+describe("example", () =>
+{
+    it("of exhaustiveness checking", () =>
+    {
+        const opt = someOperation();
+        switch (opt.state)
+        {
+            case "some":
+                break;
+
+            case "none":
+                break;
+
+            default:
+                return assertNever(opt);
+        }
+    });
+});
+
+
+describe("someOption()", () =>
+{
+    it("returns an object describing an option with a value", () =>
+    {
+        const opt = someOption(5);
+        expect(opt.state).toEqual("some");
+        expect(opt.value).toEqual(5);
+    });
+});
+
+
+describe("noneOption()", () =>
+{
+    it("returns an object describing an option with no value", () =>
+    {
+        const opt = noneOption();
+        expect(opt.state).toEqual("none");
+        expect(opt.value).toEqual(undefined);
+    });
+});
+
+
+describe("isSome()", () =>
+{
+    it("returns true when given an option with a value", () =>
+    {
+        expect(isSome(someOperation())).toBeTruthy();
+    });
+
+
+    it("returns false when given an option with no value", () =>
+    {
+        expect(isSome(noneOperation())).toBeFalsy();
+    });
+});
+
+
+describe("isNone()", () =>
+{
+    it("returns true when given an option with no value", () =>
+    {
+        expect(isNone(noneOperation())).toBeTruthy();
+    });
+
+
+    it("returns false when given an option with a value", () =>
+    {
+        expect(isNone(someOperation())).toBeFalsy();
+    });
+});
+
+
+describe("bindOption", () =>
+{
+    it("with none input the option is passed along and the function is not invoked", () =>
+    {
+        let numInvocations = 0;
+        const fn = (x: number) => { numInvocations++; return someOption(x + 1); };
+
+        const opt = bindOption(fn, noneOption());
+        expect(isNone(opt)).toBeTruthy();
+        expect(numInvocations).toEqual(0);
+    });
+
+
+    it("with some input the function is invoked and the returned option is returned", async () =>
+    {
+        let numInvocations = 0;
+        const fn = (x: number) => { numInvocations++; return someOption(x + 1); };
+
+        const opt = bindOption(fn, someOption(3));
+        expect(isSome(opt)).toBeTruthy();
+        expect(opt.value).toEqual(4);
+        expect(numInvocations).toEqual(1);
+    });
+
+
+    it("can be used easily with pipe()", async () =>
+    {
+        const subtract1 = (x: number) => x <= 0 ? noneOption() : someOption(x - 1);
+
+        const opt1 = pipe(
+            2,
+            subtract1,                              // 1
+            (o) => bindOption(subtract1, o),        // 0
+            (o) => bindOption(subtract1, o)         // none
+        );
+        expect(isNone(opt1)).toBeTruthy();
+
+        const opt2 = pipe(
+            2,
+            subtract1,                              // 1
+            (o) => bindOption(subtract1, o)         // 0
+        );
+        expect(isSome(opt2)).toBeTruthy();
+        expect(opt2.value).toEqual(0);
+
+    });
+});
+
+
+describe("mapSome()", () =>
+{
+    it("with none input the option is passed along and the function is not invoked", () =>
+    {
+        let numInvocations = 0;
+        const add1 = (x: number) => { numInvocations++; return x + 1; };
+
+        const opt = mapSome(add1, noneOption());
+        expect(isNone(opt)).toBeTruthy();
+        expect(numInvocations).toEqual(0);
+    });
+
+
+    it("with some input the function is invoked and its return values is wrapped in a some option", () =>
+    {
+        let numInvocations = 0;
+        const add1 = (x: number) => { numInvocations++; return x + 1; };
+
+        const opt = mapSome(add1, someOption(3));
+        expect(isSome(opt)).toBeTruthy();
+        expect(opt.value).toEqual(4);
+        expect(numInvocations).toEqual(1);
+    });
+
+
+    it("can be used easily with pipe()", () =>
+    {
+        const add1 = (x: number) => x + 1;
+
+        const opt = pipe(
+            someOption(3),
+            (o) => mapSome(add1, o),        // 4
+            (o) => mapSome(add1, o),        // 5
+            (o) => mapSome(add1, o)         // 6
+        );
+
+        expect(isSome(opt)).toBeTruthy();
+        expect(opt.value).toEqual(6);
+    });
+});
---
+++ src/option.ts
@@ -0,0 +1,136 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Option Types
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Describes an optional value that has a value.
+ */
+export interface ISomeOption<T>
+{
+    state: "some";
+    value: T;
+}
+
+
+/**
+ * Describes an optional value that does not have a value.
+ */
+export interface INoneOption
+{
+    state: "none";
+    value: undefined;
+}
+
+const noneVal: INoneOption = { state: "none", value: undefined };
+
+
+/**
+ * Describes an optional value that may or may not have a value.
+ */
+export type Option<T> = ISomeOption<T> | INoneOption;
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Creation Convenience Functions
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Convenience function that creates an Option that has a value.
+ * @param value The value of the option
+ * @returns The created "some" Option.
+ */
+export function someOption<T>(value: T): ISomeOption<T>
+{
+    return {state: "some", value: value};
+}
+
+
+/**
+ * Convenience function that creates an Option that does not have a value.
+ * @returns The created "none" Option.
+ */
+export function noneOption(): INoneOption
+{
+    return noneVal;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// User-Defined Type Guards
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * User-defined type guard that determines if an Option has a value.
+ * @param option - The Option to inspect
+ * @returns Whether the Option instance has a value.
+ */
+export function isSome<T>(option: Option<T>): option is ISomeOption<T>
+{
+    return option.state === "some";
+}
+
+
+/**
+ * User-defined type guard that determines if an Option does not have a value.
+ * @param option The Option to inspect
+ * @returns Whether the Option instance does not have a value.
+ */
+export function isNone<T>(option: Option<T>): option is INoneOption
+{
+    return option.state === "none";
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * If _input_ is "some", unwraps the value and passes it into _fn_.
+ * @param fn - The function to invoke on _input.value_ when _input_ is "some"
+ * value.
+ * @param input - The input Option.
+ * @returns Either the passed-through "none" option or the "some" option
+ * returned from _fn_.
+ */
+export function bindOption<TInput, TOutput>(
+    fn: (x: TInput) => Option<TOutput>,
+    input: Option<TInput>
+): Option<TOutput>
+{
+    if (isSome(input)) {
+        const ret = fn(input.value);
+        return ret;
+    }
+    else {
+        return input;
+    }
+}
+
+
+/**
+ * When _input_ is "some", maps the wrapped value using _fn_.
+ * @param fn - Function that maps the wrapped optional value to another optional
+ * value.
+ * @param input - The input Option.
+ * @returns Either the mapped "some" option or the passed through "none" option.
+ */
+export function mapSome<TInput, TOutput>(
+    fn: (x: TInput) => TOutput,
+    input: Option<TInput>
+): Option<TOutput>
+{
+    if (isSome(input)) {
+        const ret = fn(input.value);
+        return someOption(ret);
+    }
+    else {
+        return input;
+    }
+}

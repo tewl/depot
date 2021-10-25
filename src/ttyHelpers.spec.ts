e7ee93c8c3f9ea7c36c9c46624eb2c9809152f30
Kevin Peters <kwpeters@ra.rockwell.com>
2021-10-24 22:49:16 -0400

    Created hr().

---
+++ src/ttyHelpers.spec.ts
@@ -0,0 +1,9 @@
+import { hr } from "./ttyHelpers";
+
+describe("hr()", () =>
+{
+    it("returns a horizontal rule string", () =>
+    {
+        expect(hr("-").length).toBeGreaterThan(0);
+    });
+});
---
+++ src/ttyHelpers.ts
@@ -0,0 +1,9 @@
+import { repeat } from "./stringHelpers";
+
+
+export function hr(str: string): string
+{
+    const cols = process.stdout.columns ?? 80;
+    const hr = repeat(str, cols);
+    return hr;
+}

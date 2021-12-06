4caaff2528d2658544131501c40dbb993f905ddf
Kevin Peters <kwpeters@ra.rockwell.com>
2021-12-06 10:13:51 -0500

    Made mapped types homomorphic using Pick<>.

---
+++ src/typeUtils.spec.ts
@@ -0,0 +1,75 @@
+import { MakePropsNonNullable, MakePropsNonNullableAndRequired, MakePropsOptional, MakePropsRequired } from "./typeUtils";
+
+interface IAllRequired
+{
+    propA: number;
+    propB: boolean;
+    propC: string;
+    propD: Promise<number>;
+}
+
+type SomeOptional = MakePropsOptional<IAllRequired, "propB" | "propC">;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MakePropsOptional
+////////////////////////////////////////////////////////////////////////////////
+
+const test1: SomeOptional = {
+    propA: 3,                   // Required - if removed, will not compile
+    // propB                    // optional - absent
+    propC: "foo",               // optional - present
+    propD: Promise.resolve(5)   // Required - if removed, will not compile
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MakePropsRequired
+////////////////////////////////////////////////////////////////////////////////
+
+type AllRequired2 = MakePropsRequired<SomeOptional, "propC">;
+
+const test2: AllRequired2 = {
+    propA: 3,                   // Required - if removed, will not compile
+    // propB                    // optional - absent
+    propC: "foo",               // Required - if removed, will not compile
+    propD: Promise.resolve(5)   // Required - if removed, will not compile
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MakePropsNonNullable
+////////////////////////////////////////////////////////////////////////////////
+
+interface ISomeNullable {
+    propA: number;
+    propB: undefined | number;
+    propC: null | number;
+}
+
+type NoNulls = MakePropsNonNullable<ISomeNullable, "propB" | "propC">;
+
+const test3: NoNulls = {
+    propA: 3,                   // Required - if removed, will not compile
+    propB: 3,                   // Must be number.  Will not compile if undefined
+    propC: 3                    // Must be number.  Will not compile if null
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MakePropsNonNullableAndRequired
+////////////////////////////////////////////////////////////////////////////////
+
+interface ISomeOptionalAndNullable {
+    propA: number;
+    propB?: undefined | number;
+    propC?: null | number;
+}
+
+type AllRequiredNoNulls = MakePropsNonNullableAndRequired<ISomeOptionalAndNullable, "propB" | "propC">;
+
+const test4: AllRequiredNoNulls = {
+    propA: 3,                   // Required - if removed, will not compile
+    propB: 3,                   // Must be number.  Will not compile if undefined
+    propC: 3                    // Must be number.  Will not compile if null
+};
--- src/typeUtils.ts
+++ src/typeUtils.ts
@@ -2,26 +2,26 @@
  * For a type T adds optionality for each property name in TRequiredKeys.
  * TRequiredKeys can be a single property name or a union of property names.
  */
-export type MakePropsOptional<T, TOptionalKeys extends keyof T> = {
-  // All keys not in TOptionalKeys will remain as-is.
-  [X in Exclude<keyof T, TOptionalKeys>]: T[X]
-} & {
-  // All keys in TOptionalKeys, will become optional.
-  [P in TOptionalKeys]?: T[P]
-};
+export type MakePropsOptional<T, TOptionalKeys extends keyof T> =
+    // Pick all keys not in TOptionalKeys so they are left as-is.
+    Pick<T, Exclude<keyof T, TOptionalKeys>> &
+    {
+        // All keys in TOptionalKeys, will become optional.
+        [P in TOptionalKeys]?: T[P]
+    };
 
 
 /**
  * For a type T removes the optionality for each property name in TRequiredKeys.
  * TRequiredKeys can be a single property name or a union of property names.
  */
-export type MakePropsRequired<T, TRequiredKeys extends keyof T> = {
-  // All keys not in TRequiredKeys will remain as-is.
-  [X in Exclude<keyof T, TRequiredKeys>]: T[X]
-} & {
-  // All keys in TRequiredKeys, will become required (i.e. lose their optionality).
-  [P in TRequiredKeys]-?: T[P]
-};
+export type MakePropsRequired<T, TRequiredKeys extends keyof T> =
+    // Pick all keys not in TRequiredKeys so they are left as-is.
+    Pick<T, Exclude<keyof T, TRequiredKeys>> &
+    {
+        // All keys in TRequiredKeys, will become required (i.e. lose their optionality).
+        [P in TRequiredKeys]-?: T[P]
+    };
 
 
 /**
@@ -29,13 +29,13 @@ export type MakePropsRequired<T, TRequiredKeys extends keyof T> = {
  * in TNonNullableKeys. TNonNullableKeys can be a single property name or a
  * union of property names.
  */
-export type MakePropsNonNullable<T, TNonNullableKeys extends keyof T> = {
-  // All keys not in TNonNullableKeys will remain as-is.
-  [X in Exclude<keyof T, TNonNullableKeys>]: T[X]
-} & {
-  // All keys in TNonNullableKeys, will become non-nullable.
-  [P in TNonNullableKeys]: NonNullable<T[P]>
-};
+export type MakePropsNonNullable<T, TNonNullableKeys extends keyof T> =
+    // Pick all keys not in TNonNullableKeys so they are left as-is.
+    Pick<T, Exclude<keyof T, TNonNullableKeys>> &
+    {
+        // All keys in TNonNullableKeys, will become non-nullable.
+        [P in TNonNullableKeys]: NonNullable<T[P]>
+    };
 
 
 /**

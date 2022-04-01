f36559da2b89fe2073cc958c8145e0b4de6f9ec3
Kevin Peters <kwpeters@ra.rockwell.com>
2022-04-01 09:41:05 -0400

    Added more unit tests for findVertex().

--- src/directedGraph.spec.ts
+++ src/directedGraph.spec.ts
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 import { DirectedGraph } from "./directedGraph";
 import { failed, succeeded } from "./result";
 
@@ -58,14 +59,86 @@ describe("DirectedGraph()", () =>
             {
                 const comparei = (a: string, b: string) => a.toUpperCase() === b.toUpperCase();
 
+                const digraph = DirectedGraph.create(vertices, edges).value!;
+                const vw = digraph.findVertex((vert) => comparei("W", vert));
+                expect(vw).toEqual("w");
+            });
+
+
+            it("returns the found vertex (function.bind() example)", () =>
+            {
+                const comparei = (a: string, b: string) => a.toUpperCase() === b.toUpperCase();
+
                 // Example of using function.bind().
                 const digraph = DirectedGraph.create(vertices, edges).value!;
                 const vt = digraph.findVertex(comparei.bind(null, "T"));
                 expect(vt).toEqual("t");
+            });
 
-                // Example of using a plain old lambda.
-                const vw = digraph.findVertex((vert) => comparei("W", vert));
-                expect(vw).toEqual("w");
+
+            it("returns the found vertex", () =>
+            {
+                const comparei = (a: string, b: string) => a.toUpperCase() === b.toUpperCase();
+
+                const createFindFn = (vertex: string) =>
+                {
+                    function findFn(curVertex: string)
+                    {
+                        return comparei(vertex, curVertex);
+                    }
+                    return findFn;
+                };
+
+                const digraph = DirectedGraph.create(vertices, edges).value!;
+                const vt = digraph.findVertex(createFindFn("T"));
+                expect(vt).toEqual("t");
+            });
+
+
+            it("returns the found vertex (_.isEqual example)", () =>
+            {
+                // Convert the vertices into objects.  To facilitate this create
+                // a map of name to vertex object.
+                const vertexMap =
+                    new Map(
+                        Array.from(vertices.values())
+                        .map((vertName) => [vertName, {name: vertName}] as const)
+                    );
+                const objVertices = new Set(Array.from(vertexMap.values()));
+                const objEdges = edges.map((curEdge) => ({
+                    fromVertex: vertexMap.get(curEdge.fromVertex)!,
+                    toVertex:   vertexMap.get(curEdge.toVertex)!,
+                    edgeAttr:   curEdge.edgeAttr
+                }));
+                const digraph = DirectedGraph.create(objVertices, objEdges).value!;
+
+                // _.isEqual performs a deep comparison.
+                const vw = digraph.findVertex((v) => _.isEqual({name: "w"}, v));
+                expect(vw).toEqual(vertexMap.get("w"));
+            });
+
+
+            it("returns the found vertex (_.matches example)", () =>
+            {
+                // Convert the vertices into objects that have properties we
+                // will be uninterested in when finding a vertex.
+                const vertexMap =
+                    new Map(
+                        Array.from(vertices.values())
+                        .map((vertName) => [vertName, {name: vertName, dontCareA: "foo", dontCareB: "bar" }] as const)
+                    );
+                const objVertices = new Set(Array.from(vertexMap.values()));
+                const objEdges = edges.map((curEdge) => ({
+                    fromVertex: vertexMap.get(curEdge.fromVertex)!,
+                    toVertex:   vertexMap.get(curEdge.toVertex)!,
+                    edgeAttr:   curEdge.edgeAttr
+                }));
+                const digraph = DirectedGraph.create(objVertices, objEdges).value!;
+
+                // _.matches will only compare the specified properties.
+                const vw = digraph.findVertex(_.matches({name: "w"}));
+                expect(vw).toEqual(vertexMap.get("w"));
+
             });
 
 

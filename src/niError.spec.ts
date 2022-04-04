ffed11789650add1ec32483335bad4afb7f84279
Kevin Peters <kwpeters@ra.rockwell.com>
2022-04-04 12:49:30 -0400

    Created NIError.

---
+++ src/niError.spec.ts
@@ -0,0 +1,112 @@
+import { failed, failedResult, Result } from "./result";
+import { assertNever } from "./never";
+import { NIError } from "./niError";
+
+
+fdescribe("NIError", () =>
+{
+    describe("Usage", () =>
+    {
+
+        describe("with string error codes", () =>
+        {
+            function buildCastle1():
+                Result<void,
+                    NIError<"BurnedDown"> | NIError<"FellOver"> | NIError<"SankIntoSwamp">>
+            {
+                return failedResult( new NIError("SankIntoSwamp"));
+            }
+
+
+            it("does exhaustiveness checking", () =>
+            {
+                const res = buildCastle1();
+                if (failed(res))
+                {
+                    if (res.error.code === "BurnedDown")
+                    {
+                        // eslint-disable-next-line no-empty
+                    }
+                    else if (res.error.code === "FellOver")
+                    {
+                        // eslint-disable-next-line no-empty
+                    }
+                    else if (res.error.code === "SankIntoSwamp")
+                    {
+                        // eslint-disable-next-line no-empty
+                    }
+                    else
+                    {
+                        // If any of the above cases are omitted, the following code
+                        // will fail to compile.
+                        assertNever(res.error);
+                    }
+                }
+            });
+        });
+
+
+        describe("with enum error codes", () =>
+        {
+            enum BuildCastleError
+            {
+                BurnedDown,
+                FellOver,
+                SankIntoSwamp
+            }
+
+            function buildCastle2():
+                Result<void, NIError<BuildCastleError>>
+            {
+                return failedResult(new NIError(BuildCastleError.FellOver, "Optional error details."));
+            }
+
+
+            it("does exhaustiveness checking", () =>
+            {
+                const res = buildCastle2();
+                if (failed(res))
+                {
+                    if (res.error.code === BuildCastleError.BurnedDown)
+                    {
+                        // eslint-disable-next-line no-empty
+                    }
+                    else if (res.error.code === BuildCastleError.FellOver)
+                    {
+                        // eslint-disable-next-line no-empty
+                    }
+                    else if (res.error.code === BuildCastleError.SankIntoSwamp)
+                    {
+                        // eslint-disable-next-line no-empty
+                    }
+                    else
+                    {
+                        // If any of the above cases are omitted, the following code
+                        // will fail to compile.
+                        assertNever(res.error.code);
+                    }
+                }
+            });
+        });
+    });
+
+
+    describe("instnace", () =>
+    {
+        describe("toString()", () =>
+        {
+            it("Produces expected string when there is no message", () =>
+            {
+                const errA = new NIError("ERR_A");
+                expect(errA.toString()).toEqual("Error ERR_A");
+            });
+
+
+            it("Produces expected string when there is a message", () =>
+            {
+                const errA = new NIError("ERR_A", "Specific message.");
+                expect(errA.toString()).toEqual("Error ERR_A: Specific message.");
+            });
+        });
+    });
+});

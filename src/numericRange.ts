403bcc176a390cfceae3462b4c924b60cfb36ce0
Kevin Peters <kwpeters@ra.rockwell.com>
2022-06-08 09:13:51 -0400

    Added numeric ranges.

---
+++ src/numericRange.ts
@@ -0,0 +1,17 @@
+export const sInt8Min =            -128;
+export const sInt8Max =             127;
+
+export const uInt8Min =               0;
+export const uInt8Max =             255;
+
+export const sInt16Min =        -32_768;
+export const sInt16Max =         32_767;
+
+export const uInt16Min =              0;
+export const uInt16Max =         65_535;
+
+export const sInt32Min = -2_147_483_648;
+export const sInt32Max =  2_147_483_647;
+
+export const uInt32Min =             0;
+export const uInt32Max = 4_294_967_295;

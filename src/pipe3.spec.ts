53475e5880009d53b7f057c60c062f765cf7f3f8
Kevin Peters <kwpeters@ra.rockwell.com>
2022-07-31 09:48:00 -0400

    Added pipe3.

---
+++ src/pipe3.spec.ts
@@ -0,0 +1,21 @@
+import { pipe } from "./pipe3";
+
+
+
+describe("Pipe", () => {
+
+
+    it("pipes a value through the specified functions", () => {
+        const result =
+            pipe("5")
+            .pipe((str) => parseInt(str, 10))
+            .pipe((n) => n * 3)
+            .pipe((n) => n + 1)
+            .pipe((n) => n.toString())
+            .pipe((str) => str + "!")
+            .end;
+        expect(result).toEqual("16!");
+    });
+
+
+});

c1334a2e31b03cccbea196c0726f3f04cc543325
Kevin Peters <kwpeters@ra.rockwell.com>
2022-10-23 21:52:07 -0400

    Created StringI.

---
+++ src/stringI.spec.ts
@@ -0,0 +1,68 @@
+import { StringI } from "./stringI";
+
+fdescribe("StringI", () => {
+
+    describe("instance", () => {
+
+        describe("value property", () => {
+
+            it("has same case as the value used during construction", () => {
+                const si = new StringI("FooBar");
+                expect(si.value).toEqual("FooBar");
+            });
+
+        });
+
+
+        describe("equals()", () => {
+
+            describe("when given a StringI", () => {
+
+                it("returns false when the two  are different", () => {
+                    const si1 = new StringI("foo");
+                    const si2 = new StringI("bar");
+                    expect(si1.equals(si2)).toBeFalse();
+                });
+
+
+                it("returns true when the two  are identical", () => {
+                    const si1 = new StringI("FooBar");
+                    const si2 = new StringI("FooBar");
+                    expect(si1.equals(si2)).toBeTrue();
+                });
+
+
+                it("returns true when the two differ only by case", () => {
+                    const si1 = new StringI("FooBar");
+                    const si2 = new StringI("foobar");
+                    expect(si1.equals(si2)).toBeTrue();
+                });
+
+            });
+
+            describe("when given a string", () => {
+
+                it("returns false when the two  are different", () => {
+                    const si1 = new StringI("foo");
+                    expect(si1.equals("bar")).toBeFalse();
+                });
+
+
+                it("returns true when the two  are identical", () => {
+                    const si1 = new StringI("FooBar");
+                    expect(si1.equals("FooBar")).toBeTrue();
+                });
+
+
+                it("returns true when the two differ only by case", () => {
+                    const si1 = new StringI("FooBar");
+                    expect(si1.equals("foobar")).toBeTrue();
+                });
+
+            });
+
+        });
+
+    });
+
+});
---
+++ src/stringI.ts
@@ -0,0 +1,33 @@
+import { CompareResult, compareStrI } from "./compare";
+import { IEquatable } from "./equate";
+
+
+/**
+ * A string type that only performs case-insensitive comparisons.
+ */
+export class StringI implements IEquatable<StringI>, IEquatable<string> {
+
+    private readonly _strVal: string;
+
+
+    public constructor(str: string) {
+        this._strVal = str;
+    }
+
+
+    public get value(): string {
+        return this._strVal;
+    }
+
+
+    public equals(other: StringI | string): boolean {
+        if (other instanceof StringI) {
+            return compareStrI(this._strVal, other._strVal) === CompareResult.EQUAL;
+        }
+        else {
+            return compareStrI(this._strVal, other) === CompareResult.EQUAL;
+        }
+    }
+
+
+}

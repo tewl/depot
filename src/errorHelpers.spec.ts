0127b89a7e8727c0a1c48a2512d8676eb993336d
Kevin Peters <kwpeters@ra.rockwell.com>
2022-11-03 22:08:39 -0400

    Created errorToString().

---
+++ src/errorHelpers.spec.ts
@@ -0,0 +1,20 @@
+import { errorToString } from "./errorHelpers";
+
+
+describe("errorToString()", () => {
+
+    it("returns the message property when given an Error instance", () => {
+        expect(errorToString(new Error("quux"))).toEqual("quux");
+    });
+
+
+    it("returns the message property when given a non-Error object that has a string 'message' property", () => {
+        expect(errorToString({message: "foo"})).toEqual("foo");
+    });
+
+
+    it("returns the JSON representation of the error when no 'message' property is present", () => {
+        expect(errorToString({first: "Fred"})).toEqual('{"first":"Fred"}');
+    });
+
+});
---
+++ src/errorHelpers.ts
@@ -0,0 +1,20 @@
+/**
+ * Converts an error object into a string
+ *
+ * @param err - The error to be converted
+ * @returns The resulting string.  If _err_ was an Error instance, the string
+ * will be the Error's message.  Otherwise, the string is the JSON
+ * representation of _err_.
+ */
+export function errorToString(err: unknown): string {
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const errAny = err as any;
+
+    if (errAny.message && typeof errAny.message === "string") {
+        return errAny.message;
+    }
+    else {
+        return JSON.stringify(err);
+    }
+}

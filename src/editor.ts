a4382d7314463fd809a1d6f2b1b8a68d48e01696
Kevin Peters <kwpeters@ra.rockwell.com>
2023-01-04 08:53:44 -0500

    Updated openInEmacs() to allow for multiple files and dirs.

--- src/editor.ts
+++ src/editor.ts
@@ -1,18 +1,19 @@
 import { File } from "./file";
+import { Directory } from "./directory";
 import { launch } from "./launch";
 
 
 /**
  * Open the specified file in Emacs.
  *
- * @param file - The file to be opened
+ * @param filesAndDirs - The file to be opened
  * @param openInExistingEditor - Whether to reuse an existing Emacs instance.
  *     Note: You must start the Emacs server using M-x server-start for this to
  *     work.
  */
-export function openInEmacs(file: File, openInExistingEditor: boolean = false): void {
+export function openInEmacs(filesAndDirs: Array<File | Directory>, openInExistingEditor: boolean = false): void {
     let cmd: string;
-    const args: Array<string> = [];
+    let args: Array<string> = [];
 
     if (openInExistingEditor) {
         cmd = "emacsclient";
@@ -22,6 +23,7 @@ export function openInEmacs(file: File, openInExistingEditor: boolean = false): 
         cmd = "emacs";
     }
 
-    args.push(file.absPath());
+    const argsAbsPaths = filesAndDirs.map((fileOrDir) => fileOrDir.absPath());
+    args = args.concat(argsAbsPaths);
     launch(cmd, args);
 }

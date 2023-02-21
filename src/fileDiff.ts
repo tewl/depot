62d965677b49736d231c7352e8359e16848309e8
Kevin Peters <kwpeters@gmail.com>
2023-02-21 12:37:09 -0500

    Added showVsCodeDiff().

---
+++ src/fileDiff.ts
@@ -0,0 +1,24 @@
+import { insertIf } from "./arrayHelpers";
+import {File} from "./file";
+import { getOs, OperatingSystem } from "./os";
+import {spawn} from "./spawn2";
+
+
+export async function showVsCodeDiff(fileA: File, fileB: File, useExisting = false, wait = true): Promise<void> {
+
+    const cmd = getOs() === OperatingSystem.Windows ? "code.cmd" : "code";
+
+    const args = [
+        ...insertIf(!useExisting, "-n"),
+        ...insertIf(wait, "--wait"),
+        "-d",
+        fileA.toString(),
+        fileB.toString()
+    ];
+
+    console.log(args);
+
+    const spawnOut = spawn(cmd, args);
+    await spawnOut.closePromise;
+    return undefined;
+}
